a4ec4ed31615dc689b4815b6ace7ceb7ba289ce4
Antonio Santos <acamilo10@hotmail.com>
2023-02-19 16:09:46 -0300

    Adding database SQL

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.camilo.antonio.javafxjdbc {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     exports com.camilo.antonio.javafxjdbc;
     opens com.camilo.antonio.javafxjdbc to javafx.fxml;
